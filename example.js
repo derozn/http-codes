207046a72b59cf783b234cea64dbc0066e458c10
Aaron <me@4aron.com>
2019-10-17 23:24:07 +0100

    replace references with null

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 const httpCodes = require("./index.js");
 
-console.log(httpCodes.ACCEPTED);     // 200
-console.log(httpCodes.BAD_GATEWAY);  // 502
-console.log(httpCodes.BAD_REQUEST);  // 400
+console.log(httpCodes.OK);                    // 200
+console.log(httpCodes.BAD_REQUEST);           // 400
+console.log(httpCodes.INTERNAL_SERVER_ERROR); // 500
 
-console.log(httpCodes.DOESNT_EXIST); // ERROR
+console.log(httpCodes.DOESNT_EXIST);          // ERROR
